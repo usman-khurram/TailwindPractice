0b0152d3ac42a5cd140d812be46858523a2e6ddc
usman khurram <bitf20m046@pucit.edu.pk>
2024-07-29 14:24:03 +0500

    navbar hidden

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  root "tailwind#index"
+  root "tailwind#view"
+  get "/before" , to:"tailwind#index"
 end
